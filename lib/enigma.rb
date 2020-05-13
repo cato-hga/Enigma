2731123455a8c94e992f6bc866aff747318892dc
John Cato <jchristopher.cato@gmail.com>
2020-05-12 23:40:32 -0400

    Start work on encrypting method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,15 @@
 require 'date'
 
 class Enigma
+  attr_reader :message, :key, :date, :encrypted_message, :character_set
 
+  def initialize
+    @character_set = ('a'..'z')
+  end
+
+  def encrypt(message, key, date)
+    message = message.downcase
+    key = rand(00000..99999).to_s
+    date = "040895"
+  end
 end
