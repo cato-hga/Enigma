ce27d1d5ab09912e848311b206b7fa7c78f6dc4a
John Cato <jchristopher.cato@gmail.com>
2020-05-14 10:03:21 -0400

    Organize offset class.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,10 +3,6 @@ require 'date'
 class Enigma
   attr_reader :message, :key, :date, :encrypted_message, :character_set
 
-  def initialize
-    @character_set = ('a'..'z')
-  end
-
   def encrypt(message, key, date)
     message = message.downcase
     key = rand(00000..99999).to_s
--- lib/offset.rb
+++ lib/offset.rb
@@ -1,17 +1,23 @@
 require 'date'
+require 'pry'
 
 class Offset
-  attr_reader :key, :date, :formatted_key
+  attr_reader :key, :date
+
+  def initialize(key, date)
+    @key = key
+    @date = date
+  end
 
   def format_key(key)
     split_key_in_array = key.chars
-    @formatted_key = split_key_in_array.each_cons(2).map { |group| group.join.to_i }
+    formatted_key = split_key_in_array.each_cons(2).map { |group| group.join.to_i }
   end
 
   def format_offset_date(date)
     format_date = Date.parse(date).strftime("%d%m%y")
     square_of_date = format_date.to_i * format_date.to_i
-    @last_four_digits = square_of_date.to_s.chars.last(4).map(&:to_i)
+    last_four_digits = square_of_date.to_s.chars.last(4).map(&:to_i)
   end
 
   def combine(key, date)
